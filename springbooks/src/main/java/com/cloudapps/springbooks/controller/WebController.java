a5ada0fbbf46dfdffaedde577d34e935ab7f7ca7
david-rojo <davidrojotech@gmail.com>
2020-12-04 01:53:34 +0100

    index page first approach with Mustache

--- springbooks/src/main/java/com/cloudapps/springbooks/controller/WebController.java
+++ springbooks/src/main/java/com/cloudapps/springbooks/controller/WebController.java
@@ -1,5 +1,8 @@
 package com.cloudapps.springbooks.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.cloudapps.springbooks.model.api.BookSummaryResponse;
 import com.cloudapps.springbooks.service.BookService;
 import com.cloudapps.springbooks.service.CommentService;
 
@@ -22,8 +26,10 @@ public class WebController {
 	@GetMapping("/")
 	public String showBooks(Model model, HttpSession session) {
 
-		model.addAttribute("books", bookService.findAll());
-		model.addAttribute("welcome", session.isNew());
+		List<BookSummaryResponse> books = new ArrayList<>();
+		this.bookService.findAll()
+			.forEach(book -> books.add(new BookSummaryResponse(book.getId(), book.getTitle())));
+		model.addAttribute("books", books);
 
 		return "index";
 	}
