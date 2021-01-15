97e9373391742352cf125924081cd7870468d4cf
david-rojo <davidrojotech@gmail.com>
2021-01-15 03:23:55 +0100

    old code removed

--- springbooks/src/main/java/com/cloudapps/springbooks/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cloudapps.springbooks.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cloudapps.springbooks.model.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long>{
-
-}
